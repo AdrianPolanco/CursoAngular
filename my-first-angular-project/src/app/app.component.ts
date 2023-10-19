5de2f63853103316f7bfe45c045f17fccb02d4b3
Adrián Saul Polanco Ferrer <Adrian>
2023-10-19 08:53:25 -0400

    Recreacion del repositorio

---
+++ my-first-angular-project/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'first-component',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'my-first-angular-project';
+  name='Adrian Polanco';
+}
