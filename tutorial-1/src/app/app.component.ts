d01021d36ac371f83332f10df87c26e87d65cb1c
Adrian <adferrer0611@gmail.com>
2023-11-04 23:09:51 -0400

    Tutorial 1 de Angular

---
+++ tutorial-1/src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
+
+@Component({
+  selector: 'app-root',
+  //templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  template: `
+    <main>
+      <header class="brand-name">
+        <img
+          class="brand-logo"
+          src="/assets/logo.svg"
+          alt="logo"
+          aria-hidden="true"
+        />
+        <h1 class="main-title">Homes</h1>
+      </header>
+      <section class="content">
+        <app-home></app-home>
+      </section>
+    </main>
+  `,
+})
+export class AppComponent {
+  title = 'homes';
+}
