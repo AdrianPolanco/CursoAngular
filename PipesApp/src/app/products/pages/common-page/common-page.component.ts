241f41a2e872660de457711bad3a3c549c4c03a0
Adrian <adferrer0611@gmail.com>
2024-07-26 19:02:48 -0400

    Pipes section ended

--- PipesApp/src/app/products/pages/common-page/common-page.component.ts
+++ PipesApp/src/app/products/pages/common-page/common-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-common-page',
@@ -6,5 +7,47 @@ import { Component } from '@angular/core';
   styleUrl: './common-page.component.css'
 })
 export class CommonPageComponent {
+  //i18n Select
+  name: string = "Adrian";
+  gender: "male" | "female" = "male";
+  genderMap = {
+    male: "invitarlo",
+    female: "invitarla"
+  }
 
+  changeName() {
+    this.name = "Melissa";
+    this.gender = "female";
+  }
+
+  //i18n Plural
+  clients: string[] = ["Adrian", "Melissa", "Juan", "Pedro", "Maria"];
+  clientsMap = {
+    //Si hay 0 clientes en el array, muestra este mensaje
+    "=0": "no tenemos ningún cliente esperando.",
+    //Si hay 1 cliente en el array, muestra este mensaje
+    "=1": "tenemos un cliente esperando.",
+    //Si hay más de 1 cliente en el array, muestra este mensaje, el # es el número de clientes en el array
+    "other": "tenemos # clientes esperando."
+  }
+
+  deleteClient() {
+    this.clients.pop();
+  }
+
+  //KeyValues Pipe and Json Pipe
+  person = {
+    name: "Adrian",
+    age: 22,
+    address: "Calle 123"
+  }
+
+  //Async Pipe
+  //El Async Pipe hace el cleanup de los observables y promesas, por lo que no es necesario hacerlo manualmente
+  observableTimer = interval(2000);
+  promiseTimer = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Promise resolved");
+    }, 4000);
+  });
 }
