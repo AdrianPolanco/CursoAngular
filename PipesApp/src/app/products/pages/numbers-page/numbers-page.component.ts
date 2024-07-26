217087218a0bc24ea98a25bdba92c8255b3b28ba
Adrian <adferrer0611@gmail.com>
2024-07-25 22:00:46 -0400

    Uncommon pipes page made

--- PipesApp/src/app/products/pages/numbers-page/numbers-page.component.ts
+++ PipesApp/src/app/products/pages/numbers-page/numbers-page.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-numbers-page',
   templateUrl: './numbers-page.component.html',
-  styleUrl: './numbers-page.component.css'
+  styleUrl: './numbers-page.component.css',
 })
 export class NumbersPageComponent {
-
+  price: number = 14532.542;
+  percentage: number = 0.4856;
+  currentCurrency: string = 'USD';
 }
